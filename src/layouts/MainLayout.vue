70d0b628dd51a0e6c12009e9c748a59304821b86
Amalia Ferariu <a.ferariu@esis-italia.com>
2024-01-24 11:02:00 +0100

    init

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,78 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header>
+      <q-toolbar>
+        <q-btn
+          flat
+          dense
+          round
+          icon="menu"
+          aria-label="Menu"
+          @click="toggleLeftDrawer"
+        />
+      </q-toolbar>
+      <div class="q-px-md q-pt-s q-mb-s">
+        <div class="text-h5">Todo</div>
+      </div>
+    </q-header>
+
+    <q-drawer
+      v-model="leftDrawerOpen"
+      show-if-above
+      bordered
+    >
+      <q-list padding >
+        <q-item
+          to="/"
+          exact
+          cliccable
+          v-ripple
+        >
+          <q-item-section>
+            Todo
+          </q-item-section>
+        </q-item>
+        <q-item
+          to="/help"
+          cliccable
+          v-ripple
+          exact
+        >
+          <q-item-section>
+            help
+          </q-item-section>
+        </q-item>
+      </q-list>
+    </q-drawer>
+
+    <q-page-container>
+      <keep-alive>
+        <router-view />
+      </keep-alive>
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+import { defineComponent, ref } from 'vue'
+import EssentialLink from 'components/EssentialLink.vue'
+
+export default defineComponent({
+  name: 'MainLayout',
+
+  components: {
+
+  },
+
+  setup () {
+    const leftDrawerOpen = ref(false)
+
+    return {
+      leftDrawerOpen,
+      toggleLeftDrawer () {
+        leftDrawerOpen.value = !leftDrawerOpen.value
+      }
+    }
+  }
+})
+</script>
